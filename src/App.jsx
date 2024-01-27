6448932f38aa72929cce392cce4f9c5581b6a4ee
akash <musaddekurakash@gmail.com>
2024-01-27 22:58:27 +0600

    movie details component added

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,20 @@
 import React from 'react';
 import Header from './Header';
+import SideBar from './SideBar';
+import MovieList from './Cine/MovieList';
+import Footer from './Footer';
 
 const App = () => {
   return (
     <div>
       <Header />
+      <main>
+        <div className='container grid lg:grid-cols-[218px_1fr] gap-[3.5rem]'>
+          <SideBar />
+          <MovieList />
+        </div>
+      </main>
+      <Footer />
     </div>
   );
 };
