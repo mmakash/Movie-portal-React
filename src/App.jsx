554cdca4f1bbdface782584b56ff5eda84887e32
akash <musaddekurakash@gmail.com>
2024-01-27 17:56:53 +0600

    header added

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import Header from './Header';
 
 const App = () => {
   return (
     <div>
-      <h1 className='text-3xl font-bold'>React + Tailwind Setup</h1>
+      <Header />
     </div>
   );
 };
